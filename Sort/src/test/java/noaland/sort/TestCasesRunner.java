05e62d5fec62755d9adfcf4ae9749c5a6808a379
RuipengWang <rpwang@thoughtworks.com>
2018-06-20 00:01:30 +0800

    Create custom test runner and annotation(Need to learn about JUnit4 parameterized and Java customized annotation, but not urgent not important) -- Ruipeng Wang

---
+++ Sort/src/test/java/noaland/sort/TestCasesRunner.java
@@ -0,0 +1,62 @@
+package noaland.sort;
+
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+//TODO: Not urgent && not that important: Learn customize annotation and JUnit4 Parameterized.java and create a customized annotation for test
+public class TestCasesRunner extends Runner {
+    private Class testClass;
+    public TestCasesRunner(Class testClass) {
+        super();
+        this.testClass = testClass;
+    }
+
+    @Override
+    public Description getDescription() {
+        return Description
+                .createTestDescription(testClass, "My runner description");
+    }
+
+    @Override
+    public void run(RunNotifier notifier) {
+        System.out.println("running the tests from MyRunner: " + testClass);
+        try {
+            Object testObject = testClass.newInstance();
+            for (Method method : testClass.getMethods()) {
+                if (method.isAnnotationPresent(Test.class) && !method.isAnnotationPresent(UsingTestCases.class)) {
+                    notifier.fireTestStarted(Description
+                            .createTestDescription(testClass, method.getName()));
+                    method.invoke(testObject);
+                    notifier.fireTestFinished(Description
+                            .createTestDescription(testClass, method.getName()));
+                }
+
+                if(method.isAnnotationPresent(Test.class) && method.isAnnotationPresent(UsingTestCases.class)){
+                    System.out.println("test name: " + method.getAnnotation(UsingTestCases.class).name());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    @interface TestCases{
+        String name() default "";
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    @interface UsingTestCases{
+        String name() default "";
+    }
+}
