e1d8afd384ae6e5ae7c296240bff6fa46379ebb3
RuipengWang <rpwang@thoughtworks.com>
2018-06-20 22:59:28 +0800

    Change Test Case from T[] -> T, so that it can be used in any case. -- Ruipeng Wang

--- Algorithm/src/test/java/noaland/TestCases/TestCase.java
+++ Algorithm/src/test/java/noaland/TestCases/TestCase.java
@@ -1,11 +1,11 @@
 package noaland.TestCases;
 
 public class TestCase<T>{
-    T[] input;
-    T[] expected;
+    T input;
+    T expected;
     String message;
 
-    public TestCase(T[] input, T[] expected, String message){
+    public TestCase(T input, T expected, String message){
         this.input = input;
         this.expected = expected;
         this.message = message;
--- Algorithm/src/test/java/noaland/sort/SortTest.java
+++ Algorithm/src/test/java/noaland/sort/SortTest.java
@@ -8,9 +8,9 @@ public class SortTest {
     private Integer[] expectedResult = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     private TestCase[] testCases = new TestCase[]{
-            new TestCase<>(new Integer[]{5, 3, 4, 7, 2, 8, 6, 9, 1}, expectedResult, "test case 1"),
-            new TestCase<>(new Integer[]{5, 4, 8, 7, 6, 9, 3, 2, 1}, expectedResult, "test case 2"),
-            new TestCase<>(new Integer[]{4, 1, 6, 5, 7, 8, 2, 9, 3}, expectedResult, "test case 3"),
+            new TestCase(new Integer[]{5, 3, 4, 7, 2, 8, 6, 9, 1}, expectedResult, "test case 1"),
+            new TestCase(new Integer[]{5, 4, 8, 7, 6, 9, 3, 2, 1}, expectedResult, "test case 2"),
+            new TestCase(new Integer[]{4, 1, 6, 5, 7, 8, 2, 9, 3}, expectedResult, "test case 3"),
     };
 
     @Test
